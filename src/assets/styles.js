79aa24a1a5d0f4fbd962e01c64974f80153214b5
Igna <gnachoxp@gmail.com>
2020-09-28 20:47:33 -0300

    styled-comps

---
+++ src/assets/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const bgOscuro  = ''
+const bgClaro = ''
+
+const detalle1 = ''
+const detalleClaro = ''
+const detalleOscuro = ''
+
+const txtBgOscuro = ''
+const txtBgClaro = ''
+
+
+const MiniDiv = styled.div`
+   color: ${txtBgOscuro};
+   background: ${bgOscuro};
+`
+
+export default {MiniDiv};
--- src/components/Cabecera/Cabecera.js
+++ src/components/Cabecera/Cabecera.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import MiniDiv from '../../assets/styles/';
+
+const MiniDiv = styled.div`
+   color: green;
+`
+
+const StyledCabecera = styled.header`
+   background: black;
+`
 
 export default function Cabecera() {
    return (
-      <div>
-         Header
-      </div>
-   );
+      <StyledCabecera>
+         <MiniDiv>
+            <h1>IGNA GARCIA RAVLIC</h1>
+            <h2>DESARROLLADOR</h2>
+         </MiniDiv>
+      </StyledCabecera>
+   )
 }
