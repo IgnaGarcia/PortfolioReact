f1bd94b72e3057e843ead09dd6f38b1c29feb646
Igna <gnachoxp@gmail.com>
2020-09-29 00:04:09 -0300

    header y nuevos colores opcionales

--- src/assets/styles.js
+++ src/assets/styles.js
@@ -9,7 +9,12 @@ export const fonts = {
 export const colors = {
    bgOscuro : '#373737',
    bgClaro: '#eeeeee',
-   detalle1: '#e64a19',
+   detalle1: '#fb8c00',
+   /*verde 43a047 
+   verde agua 00acc1
+   azul 039be5
+   naranja fb8c00
+   */
    detalle1Claro: '#ff7043',
    detalle1Oscuro: '#d84315',
    txtBgOscuro: '#f5f5f5',
@@ -20,29 +25,23 @@ export const breackpoints = {
    mobile: '0px',
    tablet: '576px',
    laptop: '768px',
-   desktop: '992px',
-   full: '1200px'
+   desktop: '992px'
 }
 
 export const Container = styled.div`
-  width: 100%;
-  padding-right: 15px;
-  padding-left: 15px;
+  width: 90%;
   margin-right: auto;
   margin-left: auto;
 
-  @media (min-width: ${breackpoints.tablet}){
+  @media screen and (min-width: ${breackpoints.tablet}){
      max-width: '540px';
   };
-  @media (min-width: ${breackpoints.laptop}){
+  @media screen and (min-width: ${breackpoints.laptop}){
      max-width: '720px';
   };
-  @media (min-width: ${breackpoints.desktop}){
+  @media screen and (min-width: ${breackpoints.desktop}){
      max-width: '960px';
-  };
-  @media (min-width: ${breackpoints.full}){
-     max-width: '1140px';
   };
 `
 
-export default {Container, colors, fonts}
+export default {Container, colors, fonts, breackpoints}
--- src/components/Cabecera/Cabecera.js
+++ src/components/Cabecera/Cabecera.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import {Container, colors, fonts} from '../../assets/styles';
+import { Container, colors, fonts, breackpoints } from '../../assets/styles';
 
 export const MiniDiv = styled.div`
-   color: ${colors.txtBgOscuro};
-   background: ${colors.bgOscuro};
-   font-family: ${fonts.txtFamily2};
+   border-left: .1em solid ${colors.detalle1};
+   padding: .4em;
 `
 const StyledContainer = styled(Container)``
 
 const StyledCabecera = styled.header`
-   background: black;
+   color: ${colors.txtBgOscuro};
+   background: ${colors.bgOscuro};
+   
+   @media screen and (min-width: ${breackpoints.mobile}){
+		padding: 2em;
+	};
+   @media screen and (min-width: ${breackpoints.tablet}){
+		padding: 3em;
+	};
+	@media screen and (min-width: ${breackpoints.laptop}){
+		padding: 5em;
+	};
+	@media screen and (min-width: ${breackpoints.desktop}){
+		padding: 8em;
+	};
 `
 
+const H1 = styled.h1`
+   color: ${colors.detalle1};
+   font-family: ${fonts.txtFamily2};
+`;
+
+const H2 = styled.h2`
+   font-family: ${fonts.txtFamily1};
+`;
+
 export default function Cabecera() {
-   return (
-      <StyledCabecera>
-         <StyledContainer>
-            <MiniDiv>   
-               <h1>IGNA GARCIA RAVLIC</h1>
-               <h2>DESARROLLADOR</h2>
-            </MiniDiv>
-         </StyledContainer>
-      </StyledCabecera>
-   )
+	return (
+		<StyledCabecera>
+			<StyledContainer>
+				<MiniDiv>
+					<H1>IGNA GARCIA RAVLIC</H1>
+					<H2>DESARROLLADOR</H2>
+				</MiniDiv>
+			</StyledContainer>
+		</StyledCabecera>
+	)
 }
