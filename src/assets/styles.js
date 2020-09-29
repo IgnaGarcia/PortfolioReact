5cec27b6074a6a9a67376b4531e3982af703ada5
Igna <gnachoxp@gmail.com>
2020-09-28 23:30:46 -0300

    breackpoints y container

--- src/assets/styles.js
+++ src/assets/styles.js
@@ -16,8 +16,33 @@ export const colors = {
    txtBgClaro: '#212121'
 }
 
+export const breackpoints = {
+   mobile: '0px',
+   tablet: '576px',
+   laptop: '768px',
+   desktop: '992px',
+   full: '1200px'
+}
+
 export const Container = styled.div`
-   
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+
+  @media (min-width: ${breackpoints.tablet}){
+     max-width: '540px';
+  };
+  @media (min-width: ${breackpoints.laptop}){
+     max-width: '720px';
+  };
+  @media (min-width: ${breackpoints.desktop}){
+     max-width: '960px';
+  };
+  @media (min-width: ${breackpoints.full}){
+     max-width: '1140px';
+  };
 `
 
 export default {Container, colors, fonts}
--- src/components/Cabecera/Cabecera.js
+++ src/components/Cabecera/Cabecera.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import {colors, fonts} from '../../assets/styles';
+import {Container, colors, fonts} from '../../assets/styles';
 
 export const MiniDiv = styled.div`
    color: ${colors.txtBgOscuro};
    background: ${colors.bgOscuro};
    font-family: ${fonts.txtFamily2};
 `
-const OtroAlgo = styled(MiniDiv)``
+const StyledContainer = styled(Container)``
 
 const StyledCabecera = styled.header`
    background: black;
@@ -17,12 +17,12 @@ const StyledCabecera = styled.header`
 export default function Cabecera() {
    return (
       <StyledCabecera>
-         <OtroAlgo>
+         <StyledContainer>
             <MiniDiv>   
                <h1>IGNA GARCIA RAVLIC</h1>
                <h2>DESARROLLADOR</h2>
             </MiniDiv>
-         </OtroAlgo>
+         </StyledContainer>
       </StyledCabecera>
    )
 }
