f3b477c3da03adeee7d0cad9f87ae0a1d1c3296d
Igna <gnachoxp@gmail.com>
2020-09-30 01:02:20 -0300

    inicio de footer

--- src/components/Pie/Pie.js
+++ src/components/Pie/Pie.js
@@ -5,18 +5,30 @@ import { Container, colors, fonts, breackpoints } from '../../assets/styles';
 
 import social from '../../data/socialmed.json';
 
-const StyledContainer = styled(Container)`
+
+
+const StyledPie = styled.footer`
    color: ${colors.txtBgOscuro};
    background: ${colors.bgOscuro};
+   @media ${breackpoints.mobileS}{padding: 3em 0em};
+	@media ${breackpoints.laptop}{padding: 4.5em 0em};
+	@media ${breackpoints.desktop}{padding: 7em 0em};
+`
+const Contenedor = styled(Container)`
+   font-family: ${fonts.txtFamily1};
+   span{
+      font-family: ${fonts.txtFamily2};
+      color: ${colors.detalle1};
+   }
 `
 
 export default function Pie() {
    return (
-      <Container>
-         <div>
+      <StyledPie>
+         <Contenedor>
             Redes aqui Github Linkedin
-            Desarrollado por <span /*cambiocoloryfuente*/>IGNA GARCIA RAVLIC</span>
-         </div>
-      </Container>
+            Desarrollado por <span>IGNA GARCIA RAVLIC</span>
+         </Contenedor>
+      </StyledPie>
    );
 }
