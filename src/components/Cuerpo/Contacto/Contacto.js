5db29110073242dd1a67f7c137df0ba70cb08a44
Igna <gnachoxp@gmail.com>
2020-10-06 01:38:27 -0300

    incorporar json a contacto

--- src/components/Cuerpo/Contacto/Contacto.js
+++ src/components/Cuerpo/Contacto/Contacto.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import social from '../../../data/socialmed.json';
 
+
+
 export default function Contacto() {
 	return (
 		<div>
-			Redes en general y seccion para formulario
+			{social.map(x => {
+            return <div><a href={x.url}><i className={x.icon}></i></a><span>{x.name}</span></div>
+         })}
 		</div>
 	);
 }
