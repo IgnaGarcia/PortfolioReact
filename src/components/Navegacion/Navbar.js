08995e03fa3d7d56e995119b260d0fdca33a5d1c
Igna <gnachoxp@gmail.com>
2020-10-07 03:54:35 -0300

    fix grilla de links nav en mobile

--- src/components/Navegacion/Navbar.js
+++ src/components/Navegacion/Navbar.js
@@ -28,6 +28,15 @@ const Nav = styled.nav`
    @media ${breackpoints.mobileS}{
       display: ${props => (props.open ? "flex" : "none")};
       flex-direction: column;
+      a{
+         display: flex;
+         
+         i{flex: 1 0 30%;}
+         span{
+            text-align:left;
+            flex: 2 0 70%;
+         }
+      }
    };
    @media ${breackpoints.laptop}{
       width: 100%;
@@ -40,11 +49,11 @@ const Nav = styled.nav`
 function Navbar({ open }) {
   return (
    <Nav open={open}>
-      <NavLink exact strict activeClassName='is-active' to="/"><i className="fas fa-address-book"></i>Inicio</NavLink>
-      <NavLink exact strict activeClassName='is-active' to="/Trayectoria"><i className="fas fa-project-diagram"></i>Trayectoria</NavLink>
-      <NavLink exact strict activeClassName='is-active' to="/Habilidades"><i className="fas fa-brain"></i>Habilidades</NavLink>
-      <NavLink exact strict activeClassName='is-active' to="/Proyectos"><i className="fas fa-folder-open"></i>Proyectos</NavLink>
-      <NavLink exact strict activeClassName='is-active' to="/Contacto"><i className="fas fa-comments"></i>Contacto</NavLink>
+      <NavLink exact strict activeClassName='is-active' to="/"><i className="fas fa-address-book"></i><span>Inicio</span></NavLink>
+      <NavLink exact strict activeClassName='is-active' to="/Trayectoria"><i className="fas fa-project-diagram"></i><span>Trayectoria</span></NavLink>
+      <NavLink exact strict activeClassName='is-active' to="/Habilidades"><i className="fas fa-brain"></i><span>Habilidades</span></NavLink>
+      <NavLink exact strict activeClassName='is-active' to="/Proyectos"><i className="fas fa-folder-open"></i><span>Proyectos</span></NavLink>
+      <NavLink exact strict activeClassName='is-active' to="/Contacto"><i className="fas fa-comments"></i><span>Contacto</span></NavLink>
    </Nav>
   );
 }
