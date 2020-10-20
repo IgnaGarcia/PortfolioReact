ed2568c79daaff3cc911357bba09ada27d9df808
Igna <gnachoxp@gmail.com>
2020-10-20 03:17:54 -0300

    advanced skill cards

--- src/components/Cuerpo/Habilidades/Habilidades.js
+++ src/components/Cuerpo/Habilidades/Habilidades.js
@@ -2,13 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 
 import SkillCard from './SkillCard';
+import { Container, colors } from '../../../assets/styles';
+
 import skills from '../../../data/skills.json';
+
+
+const Background = styled.div`
+   background: ${colors.bgClaro};
+   padding: 1.5em 0em;
+   ${Container}{
+      padding: 1em 0em;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+   }
+`
 
 export default function Habilidades() {
 
 	return (
-		<div>
-         {skills.map( x => <SkillCard skill={x} ></SkillCard>)}
-		</div>
+		<Background>
+         <Container>
+            {skills.map( x => <SkillCard skill={x} ></SkillCard>)}
+         </Container>
+		</Background>
 	);
 }
--- src/components/Cuerpo/Habilidades/SkillCard.js
+++ src/components/Cuerpo/Habilidades/SkillCard.js
@@ -1,14 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { fonts, colors, breackpoints } from '../../../assets/styles';
+
+const Card = styled.div`
+   display: flex;
+   align-items: center;
+   border-radius: .5em;
+   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+   border: 1px solid white;
+   background: ${colors.bgGris4};
+   margin: .4em;
+   font-family: ${fonts.txtFamily1};
+   @media ${breackpoints.mobileS}{
+      flex: 1 1 25%;
+   };
+   @media ${breackpoints.laptop}{
+      flex: 0 0 25%;
+   };
+
+   i{
+      font-size: 2.5em;
+      text-align: center;
+      flex: 0 0 25%;
+      padding: .2em;
+      color: ${colors.bgGris1};
+      border-radius: .2em 0 0 .2em;
+      background: ${colors.detalle1};
+   }
+`
+
+const Content = styled.div`
+   flex: 1 0 auto;
+   background: ${colors.bgGris1};
+   color: ${colors.detalle1};
+   padding: .3em;
+   border-radius: 0 .5em .5em 0;
+`
+
 export default function SkillCard(props) {
    const skill = props.skill
+   const currentTime = new Date()
+   const experience = function(level){
+      if(level === 0) return "Principiante"
+      else if(level === 1) return "Avanzado"
+      else return "Experimentado"
+   }
+
 	return (
-		<div>
+		<Card>
          <i className={skill.icon}></i>
-         <div>
-            
-         </div>
-		</div>
+         <Content>
+            <div> {skill.name} </div>
+            <div> {experience(skill.level)} </div>
+            <div> {currentTime.getFullYear() - skill.years + 1} años de uso</div>
+         </Content>
+		</Card>
 	);
 }
