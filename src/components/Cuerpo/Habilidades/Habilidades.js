ef2a805868b5b9071e94a9e5a44bf7b0a7307eb3
Igna <gnachoxp@gmail.com>
2020-10-23 22:40:01 -0300

    add text to skills page

--- src/components/Cuerpo/Habilidades/Habilidades.js
+++ src/components/Cuerpo/Habilidades/Habilidades.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import SkillCard from './SkillCard';
-import { Container, colors, breackpoints } from '../../../assets/styles';
+import { Container, colors, breackpoints, fonts } from '../../../assets/styles';
 
 import skills from '../../../data/skills.json';
 
@@ -21,6 +21,25 @@ const Background = styled.div`
    }
 `
 
+const Text = styled.div`
+   text-align: center;
+   margin: .5em;
+   flex: 2 0 100%;
+   max-width: 80%;
+   span{
+      
+      font-size: 2em;
+      font-family: ${fonts.txtFamily2};
+      color: ${colors.bgGris2};
+   }
+   p{
+      margin: 1em;
+      font-size: 1.15em;
+      font-family: ${fonts.txtFamily1};
+      color: ${colors.txtBgClaro};
+   }
+`
+
 export default function Habilidades() {
    function compare(a, b){
       if(a.type < b.type) return -1
@@ -33,8 +52,13 @@ export default function Habilidades() {
    }
    console.log(skills.sort(compare))
 	return (
-		<Background>
+		<Background> 
          <Container>
+            <Text>
+               <span>CON QUE TRABAJO?</span>
+               <p>Aqui podras ver las tecnologias y lenguajes que manejo, aclarando el nivel de 
+                  experiencia de cada uno y el tiempo que paso desde mi primera interaccion con tal</p>
+            </Text>
             {skills.map( x => <SkillCard skill={x} ></SkillCard>)}
          </Container>
 		</Background>
