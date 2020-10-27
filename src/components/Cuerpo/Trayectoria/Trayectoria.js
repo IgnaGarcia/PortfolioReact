d3bac9bf8d1ce8be49af6458cbb81aed5202f02d
Igna <gnachoxp@gmail.com>
2020-10-26 21:44:17 -0300

    list of experience

--- src/components/Cuerpo/Trayectoria/Trayectoria.js
+++ src/components/Cuerpo/Trayectoria/Trayectoria.js
@@ -1,14 +1,40 @@
 import React from 'react';
 
+import { Container, breackpoints, colors, forts } from '../../../assets/styles';
+
 import studies from '../../../data/studies.json';
 import activities from '../../../data/otherActivities.json';
 import jobs from '../../../data/jobs.json';
 
 
 export default function Trayectoria() {
+
    return (
-      <div>
+      <Container>
          Estudios y Proyectos/Trabajos realizados
-      </div>
+         <ul> 
+            <li>
+               <h3>Experiencia Laboral:</h3>
+               <ul>
+                  {jobs.map(x => <li> {x.name} </li>)}
+               </ul>
+            </li>
+
+            <li>
+               <h3>Historia Academica:</h3>
+               <ul>
+                  {studies.map(x => <li> {x.name} </li>)}
+               </ul>
+            </li>
+
+            <li>
+               <h3>Otras Actividades:</h3>
+               <ul>
+                  {activities.map(x => <li> {x.name} </li>)}
+               </ul>
+            </li>
+            
+         </ul>
+      </Container>
    );
 }
