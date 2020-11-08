6e8caa9cfada732f9eb31077334257483c75e926
Igna <gnachoxp@gmail.com>
2020-11-08 03:04:10 -0300

    add navlinks to index

--- src/components/Cuerpo/Inicio/Inicio.js
+++ src/components/Cuerpo/Inicio/Inicio.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom'
+
 
 import { Container, breackpoints, colors, fonts } from '../../../assets/styles';
 import imagenPrueba2 from '../../../assets/attachments/SpeechToText.png';
@@ -23,9 +25,38 @@ const Content = styled.div`
    }
    @media ${breackpoints.mobileS}{
       flex: 1 0 70%;
+      a{
+         margin: 1em .3em 0em .3em;
+         padding: .5em .7em;
+      }
    }
    @media ${breackpoints.tablet}{
       flex: 0 1 45%;
+      
+   }
+   @media ${breackpoints.laptop}{
+      a{
+         padding: .7em .95em;
+         margin: 1.4em .7em 0px .7em;
+      }
+   }
+   @media ${breackpoints.desktop}{
+      a{
+         margin: 1.4em 1.2em 0em 1.2em;
+      }
+   }
+   a{
+      color: ${colors.bgGris1};
+      font-weight: lighter;
+      background: ${colors.detalle1};
+      border-radius: 10px;
+      box-shadow: 0px 2px 4px rgba(0,0,0,.25);
+      display: inline-block;
+      :hover{
+         background: ${colors.detalle1Oscuro}
+         color: ${colors.bgOscuro};
+         box-shadow: 0px 2px 6px rgba(0,0,0,.35);
+      }
    }
 `
 
@@ -76,6 +107,8 @@ export default function Inicio() {
                   pasion que se transforma en voluntad para crecer en este campo y hacerlo de la
                   mejor manera posible
                </p>
+               <NavLink exact strict to={"/Trayectoria"}> Mi Trayectoria </NavLink>
+               <NavLink exact strict to={"/Proyectos"}> Mis Proyectos </NavLink>
             </Content>
             <Imagen>
                <img className="imagen" src={imagenPrueba2} ></img>
