3633d45a72affb2ea7c5f94395f706a703b2863d
Igna Garcia <naachxp@gmail.com>
2021-03-12 13:03:29 -0300

    nuevas paletas

--- src/assets/styles.js
+++ src/assets/styles.js
@@ -1,95 +1,108 @@
-import styled from "styled-components"
-import "./fuente.css"
-import "./basics.css"
-import "./fonts/icons.css"
+import styled from "styled-components";
+import "./fuente.css";
+import "./basics.css";
+import "./fonts/icons.css";
 
 export const fonts = {
-  txtFamily1: "'Ubuntu', sans-serif",
-  txtFamily2: "'Oswald', sans-serif"
-}
+    txtFamily1: "'Ubuntu', sans-serif",
+    txtFamily2: "'Oswald', sans-serif",
+};
 
 export const colors = {
-  bgOscuro: "#373737",
-  bgGris1: "#474747",
-  bgGris2: "#777777",
-  bgGris3: "#bdbdbd",
-  bgGris4: "#e0e0e0",
-  bgClaro: "#eeeeee",
-  bgExtraClaro: "#fafafa",
-  detalle1: "#fb8c00",
-  /*verde 43a047 
-  verde agua 00acc1
-  azul 039be5
-  naranja fb8c00
+    bgOscuro: "#373737",
+    bgGris1: "#474747",
+    bgGris2: "#777777",
+    bgGris3: "#bdbdbd",
+    bgGris4: "#e0e0e0",
+    bgClaro: "#eeeeee",
+    bgExtraClaro: "#fafafa",
+    detalle1: "#1B998B",
+    detalle1Claro: "#20B590",
+    detalle1Oscuro: "#158185",
+    /*
+    verde  
+    detalle1: "#1B998B",
+    detalle1Claro: "#20B590",
+    detalle1Oscuro: "#158185",
+    
+    verde agua 00acc1
+
+    azul
+    detalle1: "#039be5",
+    detalle1Claro: "#1FC5FF",
+    detalle1Oscuro: "#007AD0",
+
+    naranja
+    detalle1: "#fb8c00",
+    detalle1Claro: "#ffa726",
+    detalle1Oscuro: "#f57c00",
    */
-  detalle1Claro: "#ffa726",
-  detalle1Oscuro: "#f57c00",
-  txtBgOscuro: "#f5f5f5",
-  txtBgOscuro2: "#e0e0e0",
-  txtBgClaro: "#212121",
-  txtBgClaro2: "#424242"
-}
+    txtBgOscuro: "#f5f5f5",
+    txtBgOscuro2: "#e0e0e0",
+    txtBgClaro: "#212121",
+    txtBgClaro2: "#424242",
+};
 
 const size = {
-  mobileS: "0px",
-  mobileM: "321px",
-  mobileL: "376px",
-  tablet: "426px",
-  laptop: "769px",
-  desktop: "1025px",
-  desktopL: "1441px"
-}
+    mobileS: "0px",
+    mobileM: "321px",
+    mobileL: "376px",
+    tablet: "426px",
+    laptop: "769px",
+    desktop: "1025px",
+    desktopL: "1441px",
+};
 
 export const breackpoints = {
-  mobileS: `(min-width: ${size.mobileS})`,
-  mobileM: `(min-width: ${size.mobileM})`,
-  mobileL: `(min-width: ${size.mobileL})`,
-  tablet: `(min-width: ${size.tablet})`,
-  laptop: `(min-width: ${size.laptop})`,
-  desktop: `(min-width: ${size.desktop})`,
-  desktopL: `(min-width: ${size.desktopL})`
-}
+    mobileS: `(min-width: ${size.mobileS})`,
+    mobileM: `(min-width: ${size.mobileM})`,
+    mobileL: `(min-width: ${size.mobileL})`,
+    tablet: `(min-width: ${size.tablet})`,
+    laptop: `(min-width: ${size.laptop})`,
+    desktop: `(min-width: ${size.desktop})`,
+    desktopL: `(min-width: ${size.desktopL})`,
+};
 
 export const Container = styled.div`
-  margin: 0em auto;
+    margin: 0em auto;
 
-  @media ${breackpoints.mobileS} {
-    max-width: "360px";
-    width: 90%;
-  }
-  @media ${breackpoints.mobileL} {
-    max-width: "400px";
-  }
-  @media ${breackpoints.tablet} {
-    max-width: "720px";
-    width: 80%;
-  }
-  @media ${breackpoints.laptop} {
-    max-width: "960px";
-  }
-  @media ${breackpoints.desktop} {
-    max-width: "1280px";
-  }
-`
+    @media ${breackpoints.mobileS} {
+        max-width: "360px";
+        width: 90%;
+    }
+    @media ${breackpoints.mobileL} {
+        max-width: "400px";
+    }
+    @media ${breackpoints.tablet} {
+        max-width: "720px";
+        width: 80%;
+    }
+    @media ${breackpoints.laptop} {
+        max-width: "960px";
+    }
+    @media ${breackpoints.desktop} {
+        max-width: "1280px";
+    }
+`;
 
 export const Titulo = styled.h2`
-  font-family: ${fonts.txtFamily2};
-  font-size: 2.5em;
-  font-weight: bold;
-  color: ${colors.txtBgClaro2 + "df"};
-  margin-bottom: 20px;
-`
+    font-family: ${fonts.txtFamily2};
+    font-size: 2.5em;
+    font-weight: bold;
+    color: ${colors.txtBgClaro2 + "df"};
+    margin-bottom: 20px;
+`;
 
 export const Parrafo = styled.p`
-  padding: 0.8em;
-  font-size: 1.15em;
-  font-family: ${fonts.txtFamily1};
-  color: ${colors.bgGris2};
-  text-align: center;
-`
+    padding: 0.8em;
+    font-size: 1.15em;
+    font-family: ${fonts.txtFamily1};
+    color: ${colors.bgGris2};
+    text-align: center;
+`;
 
 export const Text = styled.div`
-  text-align: center;
-  margin: 0.5em auto;
-  max-width: 80%;
-`
+    text-align: center;
+    margin: 0.5em auto;
+    max-width: 80%;
+`;
--- src/components/Cabecera/Cabecera.js
+++ src/components/Cabecera/Cabecera.js
@@ -1,105 +1,108 @@
 import React from "react";
 import styled from "styled-components";
-import { Fade, Slide } from 'react-reveal';
+import {Fade, Slide} from "react-reveal";
 
-import { Container, colors, fonts, breackpoints } from "../../assets/styles";
+import {Container, colors, fonts, breackpoints} from "../../assets/styles";
 
 const MiniDiv = styled.div`
-  border-left: 0.25em solid ${colors.detalle1};
-  padding: 0em 0.4em;
+    border-left: 0.25em solid ${colors.detalle1};
+    padding: 0em 0.4em;
 `;
 
 const StyledCabecera = styled.header`
-  color: ${colors.txtBgOscuro};
-  background: ${colors.bgOscuro};
+    color: ${colors.txtBgOscuro};
+    background: ${colors.bgOscuro};
 
-  @media ${breackpoints.mobileS} {
-    padding: 3em 0em;
-  }
-  @media ${breackpoints.mobileL} {
-    padding: 2.75em 0em;
-  }
-  @media ${breackpoints.tablet} {
-    padding: 3em 0em;
-  }
-  @media ${breackpoints.laptop} {
-    padding: 5em 0em;
-  }
-  @media ${breackpoints.desktop} {
-    padding: 8em 0em;
-  } ;
+    @media ${breackpoints.mobileS} {
+        padding: 3em 0em;
+    }
+    @media ${breackpoints.mobileL} {
+        padding: 2.75em 0em;
+    }
+    @media ${breackpoints.tablet} {
+        padding: 3em 0em;
+    }
+    @media ${breackpoints.laptop} {
+        padding: 5em 0em;
+    }
+    @media ${breackpoints.desktop} {
+        padding: 8em 0em;
+    } ;
 `;
 
 const H1 = styled.h1`
-  color: ${colors.txtBgOscuro2};
-  font-family: ${fonts.txtFamily2};
-  text-shadow: 3px 4px rgba(245, 124, 0, 0.75);
-  font-weight: 500;
-  @media ${breackpoints.mobileS} {
-    font-size: 4em;
-  }
-  @media ${breackpoints.mobileM} {
-    font-size: 3.75em;
-  }
-  @media ${breackpoints.mobileL} {
-    font-size: 3.5em;
-  }
-  @media ${breackpoints.tablet} {
-    font-size: 3.5em;
-    display: inline-block;
-  }
-  @media ${breackpoints.laptop} {
-    font-size: 4em;
-  }
-  @media ${breackpoints.desktop} {
-    font-size: 5em;
-  } ;
+    color: ${colors.txtBgOscuro2};
+    font-family: ${fonts.txtFamily2};
+    text-shadow: 3px 4px ${colors.detalle1Oscuro};
+    font-weight: 500;
+    @media ${breackpoints.mobileS} {
+        font-size: 4em;
+    }
+    @media ${breackpoints.mobileM} {
+        font-size: 3.75em;
+    }
+    @media ${breackpoints.mobileL} {
+        font-size: 3.5em;
+    }
+    @media ${breackpoints.tablet} {
+        font-size: 3.5em;
+        display: inline-block;
+    }
+    @media ${breackpoints.laptop} {
+        font-size: 4em;
+    }
+    @media ${breackpoints.desktop} {
+        font-size: 5em;
+    } ;
 `;
 
 const H2 = styled.h2`
-  color: ${colors.detalle1};
-  font-family: ${fonts.txtFamily1};
-  padding: 0.45em 0.1em;
-  font-weight: 300;
-  font-style: italic;
-  @media ${breackpoints.mobileS} {
-    font-size: 1.4em;
-  }
-  @media ${breackpoints.mobileM} {
-    font-size: 1.3em;
-  }
-  @media ${breackpoints.mobileL} {
-    font-size: 1.4em;
-  }
-  @media ${breackpoints.tablet} {
-    font-size: 1.2em;
-  }
-  @media ${breackpoints.laptop} {
-    font-size: 1.3em;
-  }
-  @media ${breackpoints.desktop} {
-    font-size: 1.6em;
-  } ;
+    color: ${colors.detalle1};
+    font-family: ${fonts.txtFamily1};
+    padding: 0.45em 0.1em;
+    font-weight: 300;
+    font-style: italic;
+    @media ${breackpoints.mobileS} {
+        font-size: 1.4em;
+    }
+    @media ${breackpoints.mobileM} {
+        font-size: 1.3em;
+    }
+    @media ${breackpoints.mobileL} {
+        font-size: 1.4em;
+    }
+    @media ${breackpoints.tablet} {
+        font-size: 1.2em;
+    }
+    @media ${breackpoints.laptop} {
+        font-size: 1.3em;
+    }
+    @media ${breackpoints.desktop} {
+        font-size: 1.6em;
+    } ;
 `;
 
 export default function Cabecera() {
-  return (
-    <StyledCabecera>
-      <Container>
-        <Slide top duration={800}>
-          <MiniDiv>
-            <Fade duration={2000} delay={600}>
-              <div>
-                <H1> <pre>IGNA </pre> </H1>
-                <H1> GARCIA RAVLIC </H1>
-              </div>
-            </Fade>
-            <Fade duration={2000} delay={1000}>
-              <H2>DESARROLLADOR</H2>
-            </Fade>
-          </MiniDiv>
-        </Slide>
-      </Container>
-    </StyledCabecera>
-  );
+    return (
+        <StyledCabecera>
+            <Container>
+                <Slide top duration={800}>
+                    <MiniDiv>
+                        <Fade duration={2000} delay={600}>
+                            <div>
+                                <H1>
+                                    {" "}
+                                    <pre>IGNA </pre>{" "}
+                                </H1>
+                                <H1> GARCIA RAVLIC </H1>
+                            </div>
+                        </Fade>
+                        <Fade duration={2000} delay={1000}>
+                            <H2>DESARROLLADOR</H2>
+                        </Fade>
+                    </MiniDiv>
+                </Slide>
+            </Container>
+        </StyledCabecera>
+    );
 }
